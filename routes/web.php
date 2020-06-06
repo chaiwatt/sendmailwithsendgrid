6493318130f9aec136f4b209c0b86154d6918110
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2020-06-06 15:09:34 +0700

    edit some

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::group(['prefix' => 'email'], function(){
     Route::get('send','MailController@Send')->name('mail.send');           
 }); 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
